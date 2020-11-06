4ba569bbc85de6b331f922cae2d2e2384b12195d
BH Apps <10782864+bhapps@users.noreply.github.com>
2020-11-06 15:05:06 +1000

    updates

--- utilitools-permissions/src/main/java/bhapps/utilitools/permissions/kotlin/Permissions.kt
+++ utilitools-permissions/src/main/java/bhapps/utilitools/permissions/kotlin/Permissions.kt
@@ -1,13 +1,11 @@
 package bhapps.utilitools.permissions.kotlin
 
 import android.app.Activity
-import android.app.Instrumentation
 import android.content.Context
 import android.content.pm.PackageManager
-import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.util.ArrayList
+import java.util.*
 
 class Permissions {
 
@@ -15,7 +13,10 @@ class Permissions {
 
         try {
 
-            var info = context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_PERMISSIONS)
+            var info = context.packageManager.getPackageInfo(
+                context.packageName,
+                PackageManager.GET_PERMISSIONS
+            )
             val requestedPermissions = info.requestedPermissions
             if (requestedPermissions != null && requestedPermissions.isNotEmpty()) {
                 return requestedPermissions
@@ -29,6 +30,15 @@ class Permissions {
     }
 
     fun checkAllPermissionsGrantedInArray(context: Context, requiredPermissions: Array<String?>): Boolean {
+        for (permission in requiredPermissions) {
+            if (!isPermissionGranted(context, permission)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    fun checkAllPermissionsGranted(context: Context): Boolean {
         for (permission in getRequiredPermissions(context)) {
             if (!isPermissionGranted(context, permission)) {
                 return false
@@ -68,6 +78,26 @@ class Permissions {
 
         return true
 
+    }
+
+    fun checkPermissionsStringArrayListWithBooleanResult(
+        activity: Activity?,
+        permissions_list: Array<String>
+    ): Boolean {
+        val checkedPermissionsList: MutableList<String> = ArrayList()
+        for (index in permissions_list) {
+            if (ContextCompat.checkSelfPermission(
+                    activity!!,
+                    index
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                checkedPermissionsList.add(index)
+            }
+        }
+        return if (!checkedPermissionsList.isEmpty()) {
+            false
+        } else true
     }
 
 }
